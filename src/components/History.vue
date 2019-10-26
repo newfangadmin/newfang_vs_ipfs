e6a85325f474a255631f173a0958faee47ddb1d5
SauravKanchan <sauravnk30@gmail.com>
2019-10-26 18:49:21 +0530

    Make transaction history dynamic

--- src/components/History.vue
+++ src/components/History.vue
@@ -2,9 +2,9 @@
   <div class="p-4 h-screen">
     <el-card class="h-screen tableCard">
       <el-table class="font-body"
-        :data="tableData"
-        stripe
-        style="width: 100%">
+                :data="tableData"
+                stripe
+                style="width: 100%">
         <el-table-column
           prop="actionType"
           label="Action Type"
@@ -33,39 +33,48 @@
 </template>
 
 <script>
-
-export default {
-  name: 'History',
-  data() {
-    return {
-      tableData: [{
-        actionType: 'Upload',
-        fileSize: '2.3MB',
-        ipfsTime: '112s',
-        nfTime: '23s'
-      }]
-    }
-  },
-  mounted() {
-    for(var i=0; i<20; i++) {
-      this.tableData.push({
-        actionType: 'Upload',
-        fileSize: '2.3MB',
-        ipfsTime: '112s',
-        nfTime: '23s'
-      })
+  const axios = require('axios');
+  export default {
+    name: 'History',
+    data() {
+      return {
+        tableData: []
+      }
+    },
+    methods: {
+      fileSizeFilter(val) {
+        const digitCount = val.toString().length
+        if (digitCount < 4) {
+          return (val).toFixed(2) + " B"
+        } else if (digitCount < 7) {
+          return (val / 1000).toFixed(2) + " KB"
+        } else {
+          return (val / 1000 / 1000).toFixed(2) + " MB"
+        }
+      }
+    },
+    async mounted() {
+      let res = await axios.get("http://13.232.245.32:8000/api/transactions/");
+      let data = res.data;
+      for (let i = 0; i < data.length; i++) {
+        this.tableData.push({
+          actionType: data[i].type,
+          fileSize: this.fileSizeFilter(data[i].fileSize),
+          ipfsTime: data[i].ipfsTime,
+          nfTime: data[i].nwTime
+        })
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.el-card__body {
-  padding: 0 !important;
-}
+  .el-card__body {
+    padding: 0 !important;
+  }
 
-.tableCard {
-  overflow: auto;
-}
+  .tableCard {
+    overflow: auto;
+  }
 </style>
